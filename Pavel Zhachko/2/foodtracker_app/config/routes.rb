0711cc25c3b007f8002f4ed70f2f0a7a1a3a329c
Pavel Zhachko <rammiel@yandex.ru>
2019-03-17 15:28:23 +0300

    Hounds style fixes.

--- Pavel Zhachko/2/foodtracker_app/config/routes.rb
+++ Pavel Zhachko/2/foodtracker_app/config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :dishes
   resources :ingestions
   resources :users, only: %i[new create show destroy]
-  #root 'dishes#index'
   root 'ingestions#index'
   get 'profile', to: 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
